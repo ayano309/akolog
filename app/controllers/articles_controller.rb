7ed933ae0160ae0806d6e61605731f00d4fdd734
ayano309 <ayatomato.k@gmail.com>
2022-04-09 15:05:11 +0000

    articleのcontroller作成

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @articles =Article.all
+  end
+
+  def show
+    @article = Artcle.find(params[:id])
+  end
+
+  def new
+    @article = current_user.articles.build
+  end
+  
+  def create
+     @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to article_path(@article), notice: 'Successfully　created'
+    else
+      render :new
+    end
+  end
+
+  def edit
+     @article = current_user.articles.find(params[:id])
+  end
+  
+  def update
+    @article = current_user.articles.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article), notice: 'Successfully　updated'
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+     article = current_user.articles.find(params[:id])
+     article.destroy!
+     redirect_to articles_path, notice: 'Successfully　deleted'
+  end
+  
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
