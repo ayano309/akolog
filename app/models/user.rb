bf68c41370fe456c6c95747bd84fa1caba0431f5
ayano309 <ayatomato.k@gmail.com>
2022-04-10 06:24:44 +0000

    profileモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles
+  has_one :profile, dependent: :destroy
+  
+  delegate :birthday, :gender, to: :profile, allow_nil: true
   
   def has_written?(article)
     articles.exists?(id: article.id)
+  end
+  
+  def display_name
+    profile&.nickname || self.email.split('@').first
+  end
+
+  def birthday
+    profile&.birthday
+  end
+
+  def gender
+    profile&.gender
   end
 end
